fbc5be07fce934754d10190d380a9cc928d1f06d
Rahel Gunaratne <rahel.gunaratne@gmail.com>
2021-01-02 12:13:06 -0500

    scraper initial

--- venv/Scraper.py
+++ venv/Scraper.py
@@ -60,8 +60,36 @@ from csv import DictReader
 from bs4 import BeautifulSoup as Soup
 import json
 
+pages = 10
+skills = ["magic", "fletching", "woodcutting", "firemaking", "construction", "farming", "herblore", "thieving", "mining", "smithing"]
+#skills = ["magic"]
+count = 200
 
+def appendPageNum(query):
+    for i in range(2, pages+1):
+        pagenum = {"pagenum":str(i)}
+        query.append(pagenum)
+    return query
 
+query = [{"type":"currenttop","timeperiod":"day","skill":"runecrafting","filter":"ironman>0","pagenum":"1","count":""}]
+query[0]["count"] = str(count)
+
+for skill in skills:
+    query = appendPageNum(query)
+
+    nofilter = {"filter":"", "pagenum":"1"}
+    query.append(nofilter)
+
+    query = appendPageNum(query)
+
+    newskill = {"skill":"","filter":"ironman>0","pagenum":"1"}
+    newskill["skill"] = skill
+    query.append(newskill)
+
+query = appendPageNum(query)
+nofilter = {"filter": "", "pagenum": "1"}
+query.append(nofilter)
+query = appendPageNum(query)
 
 base_url = "http://www.crystalmathlabs.com/tracker/api.php?multiquery="
 
@@ -74,88 +102,64 @@ hdr = {
     'Accept-Language': 'en-US,en;q=0.8',
     'Connection': 'keep-alive'}
 
+url = str(base_url) + str(json.dumps(query).replace(" ", ""))
 
-skills = ["runecraft", "magic", "fletching", "woodcutting", "firemaking", "construction", "farming", "herblore", "thieving", "mining", "smithing"]
+print(url)
 
+response = get(url, headers=hdr)
 
-for skill in skills:
-    query=[{"type":"currenttop","timeperiod":"day","skill":"","filter":"ironman>0","pagenum":"1","count":"30"},
-    {"pagenum":"2"},
-    {"pagenum":"3"},
-    {"pagenum":"4"},
-    {"pagenum":"5"},
-    {"pagenum":"6"},
-    {"pagenum":"7"},
-    {"pagenum":"8"},
-    {"pagenum":"9"},
-    {"pagenum":"10"},
-    {"filter":"","pagenum":"1"},
-    {"pagenum":"2"},
-    {"pagenum":"3"},
-    {"pagenum":"4"},
-    {"pagenum":"5"},
-    {"pagenum":"6"},
-    {"pagenum":"7"},
-    {"pagenum":"8"},
-    {"pagenum":"9"},
-    {"pagenum":"10"},
-    ]
+if response.text.__contains__("-4"):
+    print("API currently unavailable")
+    exit(-4)
 
-    query[0]["skill"] = skill
+def subtract(d1, d2):
+    res = dict()
+    for key in d1:
+        if key not in d2:
+            res[key] = d1[key]
+    return res
 
-    print(skill)
-    url = str(base_url) + str(json.dumps(query).replace(" ", ""))
-    print(url)
+def average(lst):
+    return sum(lst)/len(lst)
 
-    response = get(url, headers=hdr)
+groupNum = 0
+skillIndex = -1
 
-    ironmanPlayers = {}
-    allPlayers = {}
+ironmanPlayers = {}
+allPlayers = {}
 
-    pageNum = 1
-    readingIronman = True
+readingIronman = True
 
-    if response.text.__contains__("-4"):
-        exit(-4)
+xp_averages = {}
 
-    for line in response.text.splitlines():
-        print(line)
-        if line == "":
-            continue
-        elif line == "~~":
-            pageNum += 1
-            continue
-
-        if pageNum == 11:
-            pageNum = 1
+for line in response.text.splitlines():
+    if line == "":
+        continue
+    elif line == "~~":
+        groupNum += 1
+        if groupNum % (2 * pages) == 0 and groupNum != 0:
+            readingIronman = True
+            vanilla_players = subtract(allPlayers, ironmanPlayers)
+            xp_list = [int(i) for i in vanilla_players.values()]
+            print(str(skillIndex) + ": " + str(len(vanilla_players)))
+            if skillIndex == -1:
+                xp_averages["runecraft"] = average(xp_list)
+            else:
+                xp_averages[skills[skillIndex]] = average(xp_list)
+            ironmanPlayers.clear()
+            allPlayers.clear()
+            skillIndex += 1
+        elif groupNum % pages == 0 and groupNum != 0:
             readingIronman = False
+        continue
 
-        if readingIronman:
-            ironmanPlayers[line.split(",")[0]] = line.split(",")[1]
-        else:
-            allPlayers[line.split(",")[0]] = line.split(",")[1]
+    if readingIronman:
+        ironmanPlayers[line.split(",")[0]] = line.split(",")[1]
+    else:
+        allPlayers[line.split(",")[0]] = line.split(",")[1]
 
-    print(len(allPlayers))
-    print(len(ironmanPlayers))
+print(xp_averages)
 
-    def subtract(d1, d2):
-        res = dict()
-        for key in d1:
-            if key not in d2:
-                res[key] = d1[key]
-        return res
-
-    def average(lst):
-        return sum(lst)/len(lst)
-
-    vanilla_players = subtract(allPlayers, ironmanPlayers)
-
-    print(len(vanilla_players))
-    print(vanilla_players)
-
-    xp_list = [int(i) for i in vanilla_players.values()]
-
-    print(average(xp_list))
 
 
 
